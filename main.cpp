d5b457cfe49e61e651094478b4c754e0c40553cd
Sylvain Glaize <mokona@puupuu.org>
2021-08-14 17:13:51 +0200

    Simple SDL sample where everything goes fine on Ubuntu.

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <SDL2/SDL.h>
+
+int main()
+{
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window* window = SDL_CreateWindow("SDL Template Application",
+                                          SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                                          800, 600,
+                                          SDL_WINDOW_SHOWN);
+
+    bool running = true;
+
+    while (running)
+    {
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            switch (event.type)
+            {
+                case SDL_QUIT:
+                    running = false;
+                    break;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
